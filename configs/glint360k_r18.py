2961dfb3214dc22f39ac585586c9dbb4bb4653ff
yuleung <leungyu5@outlook.com>
2023-11-20 10:15:54 +0800

    Add files via upload

--- configs/glint360k_r18.py
+++ configs/glint360k_r18.py
@@ -1,15 +1,14 @@
 from easydict import EasyDict as edict
 
+# make training faster
+# our RAM is 256G
+# mount -t tmpfs -o size=140G  tmpfs /train_tmp
+
 config = edict()
 
-config.pq_branch_margin = 0.2
-config.theta1 = 1
-config.theta2 = 1
-config.pq = 8
-config.Fnor = True
-config.Wnor = True
+
 config.loss = 'cosface'
-config.network = "r50"
+config.network = "r18"
 config.orinetwork = False
 config.resume = False
 config.output = None
@@ -23,7 +22,6 @@ config.batch_size = 128
 config.lr = 0.1
 config.verbose = 5000
 config.dali = False
-config.pq_label='./feature_save/r50_glint360k_PQ8_nbit64_no_ovelap.npy'
 config.rec = "./dataset/glint360k"
 config.num_classes = 360232
 config.num_image = 17091657
